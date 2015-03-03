1ac65a635498056eb6e3b121082bf161952fbb35
Boussaid Mustafa <bousali@gmail.com>
2015-03-03 22:15:16 +0100

    3.0

--- library/Tamayouz/Listen.php
+++ library/Tamayouz/Listen.php
@@ -2,11 +2,8 @@
 class Tamayouz_Listen {
     
  public static function loclass($class, array &$extend)
-    {
-        if ($class == 'XenForo_ControllerPublic_Index')
-        {
+    {        
             $extend[] = 'Tamayouz_ControllerPublic_Index';
-        }
     }
 	
  public static function template_create($templateName, array &$params, XenForo_Template_Abstract $template){
@@ -14,8 +11,9 @@ class Tamayouz_Listen {
          $tampos = $options->tam_pos;
 		 switch ($tampos)
          {
-			case 10: $tmpl = 'tama_box';    break;
-			case 20: $tmpl = 'tama_boxx';   break;
+			case 10: $tmpl = 'bestA';    break;
+			case 20: $tmpl = 'bestB';   break;
+			case 25: $tmpl = 'bestC';   break;
          }
 			if ($templateName == 'forum_list'){
             $template->preloadTemplate($tmpl);
@@ -28,8 +26,9 @@ class Tamayouz_Listen {
          $tampos = $options->tam_pos;
 		 switch ($tampos)
          {
-			case 10: $tamp = 'tama_box';    break;
-			case 20: $tamp = 'tama_boxx';   break;
+			case 10: $tamp = 'bestA';    break;
+			case 20: $tamp = 'bestB';   break;
+			case 25: $tamp = 'bestC';   break;
          }
          if ($hookName=='forum_list_nodes'){            
             $templ = $template->create($tamp, $template->getparams());
--- library/Tamayouz/Model/Tam.php
+++ library/Tamayouz/Model/Tam.php
@@ -1,5 +1,5 @@
 <?php
-class Tamayouz_Model_tam extends XenForo_Model
+class Tamayouz_Model_Tam extends XenForo_Model
 {
 	public function getTopth()
 	{
@@ -9,63 +9,65 @@ class Tamayouz_Model_tam extends XenForo_Model
         $pest_all_perday_how = $options->pest_all_perday_how;
         $pest_all_theard_time = $options->pest_all_theard_time;
        
-        // الأقسام المستثناة
+        // Forums
         if($pest_all_perday_fourms_out!=''){
-        $outfourmsm="AND `node_id` NOT 
+        $outfourmsm="AND node_id NOT 
         IN (".$pest_all_perday_fourms_out.")";
         }
         else {$outfourmsm="";}
-        // طريقة الفرز
+        // Order
         if($pest_all_perday_how ==2){
-        $how_best="ORDER BY `xf_thread`.`view_count` DESC ";
+        $how_best="ORDER BY view_count DESC ";
         }
-        else {$how_best="ORDER BY `xf_thread`.`reply_count` DESC  "; }
+        else {$how_best="ORDER BY reply_count DESC"; }
        
-        // الاستعلام عن الموضوع المميز
+        /** Best Thread*/
         return $this->_getDb()->fetchAll("SELECT * 
         FROM xf_thread 
 		LEFT JOIN xf_user AS user ON (xf_thread.user_id = user.user_id)
         WHERE discussion_state ='visible'
-        AND discussion_open ='1' ".
-        $outfourmsm ."
+        AND discussion_open = 1
+        ".$outfourmsm ."
         AND post_date >= UNIX_TIMESTAMP() - $pest_all_theard_time  $how_best LIMIT 1 ");
     }
     
+    /** Best user */
     public function getTopuser()
-    {
-        
+    {      
         $options = XenForo_Application::get('options');
         $pest_all_perday_users = $options->pest_all_perday_users;
         $pest_all_theard_time = $options->pest_all_theard_time;
-        $db = XenForo_Application::get('db');
-        $userModel = new Xenforo_Model_User;
-        // الاستعلام عن العضو
-        return $this->_getDb()->fetchAll("SELECT *, COUNT( xp.post_id ) AS total 
-FROM xf_user AS xu, xf_post AS xp, xf_user_group AS xg
-WHERE xu.user_id = xp.user_id
-AND xg.user_group_id= '" .$pest_all_perday_users."' AND xu.user_group_id = '" .$pest_all_perday_users."'
-AND xu.is_banned ='0'
-AND xp.post_date >= UNIX_TIMESTAMP() - $pest_all_theard_time
-GROUP BY xp.user_id
-ORDER BY total DESC 
-LIMIT 1 ");
+        
+        return $this->_getDb()->fetchAll('SELECT *, COUNT( xp.post_id ) as total 
+        FROM xf_user as xu
+        LEFT JOIN xf_post as xp ON (xu.user_id = xp.user_id)
+        LEFT JOIN xf_user_profile as xup ON (xu.user_id = xup.user_id)
+        WHERE xu.user_id = xp.user_id
+        AND xu.user_group_id = ?
+        AND xu.is_banned = ?
+        AND xp.post_date >= UNIX_TIMESTAMP() - ?
+        GROUP BY xp.user_id
+        ORDER BY total DESC 
+        LIMIT ? ', array($pest_all_perday_users, 0, $pest_all_theard_time, 1));
     }
     
+    /** Best Staff */
     public function getTopmod()
     {
         
         $options = XenForo_Application::get('options');
-        $pest_all_perday_modman = $options->pest_all_perday_modman;
         $pest_all_theard_time = $options->pest_all_theard_time;
         
-        return $this->_getDb()->fetchAll("SELECT *, COUNT( * ) AS total 
-FROM xf_user AS xf_user, xf_post AS xf_post, xf_user_group AS xf_user_group
-WHERE xf_user.user_id = xf_post.user_id
-AND xf_user_group.user_group_id='" .$pest_all_perday_modman."' AND `xf_user`.`user_group_id`='" .$pest_all_perday_modman."'
-AND xf_post.post_date >= UNIX_TIMESTAMP() - $pest_all_theard_time
-GROUP BY xf_post.user_id
-ORDER BY total DESC 
-LIMIT 1 ");
+        return $this->_getDb()->fetchAll('SELECT *, COUNT( xp.post_id ) as total 
+        FROM xf_user as xu
+        LEFT JOIN xf_post as xp ON (xu.user_id = xp.user_id)
+        LEFT JOIN xf_user_profile as xup ON (xu.user_id = xup.user_id)
+        WHERE xu.user_id = xp.user_id
+        AND xu.is_staff = ? OR xu.is_moderator = ? OR xu.is_admin = ?
+        AND xp.post_date >= UNIX_TIMESTAMP() - ?
+        GROUP BY xp.user_id
+        ORDER BY total DESC 
+        LIMIT ? ', array(1, 1, 1, $pest_all_theard_time, 1));
     }
     
     public function gtPermissions()
